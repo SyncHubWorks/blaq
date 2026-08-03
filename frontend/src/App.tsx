e7023bdfabcce682e7db92985384aef7f6f8d109
Tebatso <tebatsomangena8@gmail.com>
2026-08-03 21:05:17 +0200

    Set up routing with React Router and add HomePage and AboutPage components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+
 function App() {
-  return <h1>Blaq</h1>;
+  return (
+    <>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;

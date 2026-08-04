2f1ba007d10cf87c2edee9e6be3c09a8abde2692
Tebatso <tebatsomangena8@gmail.com>
2026-08-04 04:35:05 +0200

    Refactor authentication flow: implement user login and registration with state management; update routes and API configuration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,52 @@
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassword";
 import AboutPage from "./pages/AboutPage";
 import HomePage from "./pages/HomePage";
+import { Loader2 } from "lucide-react";
+import { useEffect } from "react";
+import { useAuthStore } from "./store/useAuthStore";
+import { Toaster } from "react-hot-toast";
 
 function App() {
+  const { checkAuth, isCheckingAuth, user } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  if (isCheckingAuth) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-green-50">
+        <Loader2 className="h-10 w-10 animate-spin text-gray-400" />
+      </div>
+    );
+  }
+
   return (
-    <Routes>
-      <Route index element={<HomePage />} />
+    <>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/about" element={<AboutPage />} />
 
-      <Route path="/login" element={<Login />} />
+        {/* PROTECTED ROUTES */}
+        <Route
+          path="/login"
+          element={!user ? <Login /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/register"
+          element={!user ? <Register /> : <Navigate to="/" />}
+        />
 
-      <Route path="/register" element={<Register />} />
+        {/* BUYER ROUTES */}
 
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-
-      <Route path="/about" element={<AboutPage />} />
-    </Routes>
+        {/* FALLOUT */}
+      </Routes>
+      <Toaster />
+    </>
   );
 }
 
--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5050/api";
+const API_URL =
+  import.meta.env.MODE === "production"
+    ? "https://blaq-oyi2.onrender.com/api"
+    : "http://localhost:5050/api";
 
 export const axiosInstance = axios.create({
   baseURL: API_URL,
